5d89c1e08d90ea21993abeee1375514e4cfc8802
Abin John <abinjohnanto@gmail.com>
2022-09-08 08:53:02 +0530

    feat: add output textarea

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 
 const btnClick = document.getElementById('btn-translate');
 const inputText = document.getElementById('input-text');
+const outputText = document.getElementById('text-output');
 
 const btnClickHandler = function () {
   const input = inputText.value;
-  console.log(input);
+  outputText.value = input;
   input.value = '';
 };
 
