122d9bba94622413595e13349b2e11085a97a07f
Abin John <abinjohnanto@gmail.com>
2022-09-08 10:15:33 +0530

    feat: include basic fetch

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ const btnClick = document.getElementById('btn-translate');
 const inputText = document.getElementById('input-text');
 const outputText = document.getElementById('text-output');
 
-const btnClickHandler = function () {
+const URLGenerator = (text) =>
+  'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=' + text;
+
+const makeFetch = (url) => {
+  fetch(url)
+    .then((res) => res.json())
+    .then(
+      (json) =>
+        (outputText.value =
+          json.contents.text + '\n' + json.contents.translated)
+    );
+};
+
+const btnClickHandler = () => {
   const input = inputText.value;
-  outputText.value = input;
-  input.value = '';
+  makeFetch(URLGenerator(input));
 };
 
 btnClick.addEventListener('click', btnClickHandler);
